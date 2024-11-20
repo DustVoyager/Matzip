e291c240c1b0a84f010f10af70824192a81d8fc8
DustVoyager <ohjamdol@gmail.com>
2024-11-20 12:34:06 +0900

    패키지 버젼 호환이 안되는 이슈로 버전 수정

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin', // 이 플러그인은 반드시 맨 아래에 있어야 합니다.
+  ],
 };
