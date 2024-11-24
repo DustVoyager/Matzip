4dd3cdfac58a6b0d2472d5d1c86bfe4d0a75fed7
DustVoyager <ohjamdol@gmail.com>
2024-11-24 21:58:23 +0900

    Form Logic Refactoring & validation

--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
-import {Dimensions, Image, StyleSheet, View, ViewBase} from 'react-native';
+import {Dimensions, Image, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import {authNavigations} from '../../constans/navigations';
