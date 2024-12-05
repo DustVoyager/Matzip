2e9cf7251c0d76c567242831767b9afa6fc02647
DustVoyager <ohjamdol@gmail.com>
2024-12-05 22:21:20 +0900

    useAuth 작업중...

--- front/src/hooks/queries/useAuth.ts
+++ front/src/hooks/queries/useAuth.ts
@@ -1,21 +1,27 @@
-import {useMutation, UseMutationOptions} from '@tanstack/react-query';
-import {postSignup} from '../../api/auth';
-import {AxiosError} from 'axios';
-
-type ResponseError = AxiosError<{
-  statusCOde: string;
-  message: string;
-  error: string;
-}>;
-
-type UseMutationCustomOptions<TData = unknown, TVariables = unknown> = Omit<
-  UseMutationOptions<TData, ResponseError, TVariables, unknown>,
-  'mutationFn'
->;
+import {useMutation} from '@tanstack/react-query';
+import {postLogin, postSignup} from '../../api/auth';
+import {UseMutationCustomOptions} from '../../types/common';
+import {setEncryptStorage} from '../../utils';
+import axiosInstance from '../../api/axios';
+import {setHeader} from '../../utils/header';
 
 function useSignup(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: postSignup,
+    ...mutationOptions,
+  });
+}
+
+function useLogin(mutationOptions?: UseMutationCustomOptions) {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: ({accessToken, refreshToken}) => {
+      setEncryptStorage('refreshToken', refreshToken);
+      setHeader('Authorization', `Bearer ${accessToken}`);
+    },
+    onSettled: () => {
+      //
+    },
     ...mutationOptions,
   });
 }
