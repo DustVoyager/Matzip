76d4840562d9fcad4fa90256c167e52df4861497
DustVoyager <ohjamdol@gmail.com>
2024-12-22 22:04:48 +0900

    madi drawer update.." exit

--- front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import MapHomeScreen from '@/screens/map/MapHomeScreen';
 import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
 import CalendarHomeScreen from '@/screens/calendar/CalendarHomeScreen';
+import MapStackNavigator from '../stack/MapStackNavigator';
 
 const Drawer = createDrawerNavigator();
 
 function MainDrawerNavigator() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="MapHome" component={MapHomeScreen} />
-      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
-      <Drawer.Screen name="CalendarHome" component={CalendarHomeScreen} />
+      <Drawer.Screen
+        name="MapHome"
+        component={MapStackNavigator}
+        options={{
+          title: '홈',
+        }}
+      />
+      <Drawer.Screen
+        name="FeedHome"
+        component={FeedHomeScreen}
+        options={{
+          title: '피드',
+        }}
+      />
+      <Drawer.Screen
+        name="CalendarHome"
+        component={CalendarHomeScreen}
+        options={{
+          title: '캘린더',
+        }}
+      />
     </Drawer.Navigator>
   );
 }
