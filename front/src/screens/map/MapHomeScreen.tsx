1e3c5ac721520ce0d740a9d52a33d60b720615f1
DustVoyager <ohjamdol@gmail.com>
2024-12-25 23:08:28 +0900

    Map home screen updating

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,22 +1,45 @@
+import {colors} from '@/constans';
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Pressable, StyleSheet, Text} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
-function MapHomeScreen() {
+function MapHomeScreen({navigation}: any) {
+  const inset = useSafeAreaInsets();
   return (
-    <MapView
-      style={styles.container}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation
-      followsUserLocation
-      showsMyLocationButton={false}
-    />
+    <>
+      <MapView
+        style={styles.container}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={false}
+      />
+      <Pressable
+        style={[styles.drawerButton, {top: inset.top || 20}]}
+        onPress={() => navigation.openDrawer()}>
+        <Text>서랍</Text>
+      </Pressable>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  drawerButton: {
+    position: 'absolute',
+    left: 0,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: colors.PINK_700,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.5,
+    elevation: 4,
   },
 });
 
