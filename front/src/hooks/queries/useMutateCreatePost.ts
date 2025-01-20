407ca124a9755420f7101f79bac5349a70d52dad
DustVoyager <ohjamdol@gmail.com>
2025-01-20 10:11:23 +0900

    use mutate create post update

--- front/src/hooks/queries/useMutateCreatePost.ts
+++ front/src/hooks/queries/useMutateCreatePost.ts
@@ -1,6 +1,40 @@
-import {UseMutationCustomOptions} from '@/types/common';
 import {useMutation} from '@tanstack/react-query';
 
+import {createPost} from '@/api/post';
+
+import queryClient from '@/api/queryClient';
+import {queryKeys} from '@/constans';
+import {UseMutationCustomOptions} from '@/types/common';
+import {Marker} from '@/types/domain';
+
 function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
-  return useMutation({});
+  return useMutation({
+    mutationFn: createPost,
+    onSuccess: newPost => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        existingMarkers => {
+          const newMarker = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            color: newPost.color,
+            score: newPost.score,
+          };
+
+          return existingMarkers
+            ? [...existingMarkers, newMarker]
+            : [newMarker];
+        },
+      );
+    },
+
+    ...mutationOptions,
+  });
 }
+
+export default useMutateCreatePost;
